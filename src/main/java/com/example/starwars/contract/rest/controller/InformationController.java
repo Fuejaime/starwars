aa66258867c2bf7ee950dc526ab13475a861e815
jaimegomez <jaimegfuenmayor@gmail.com>
2024-07-31 21:32:01 +0200

    git commit -m "Add Basic Cacheable- If is necesary we can add  a CacheManager"

--- src/main/java/com/example/starwars/contract/rest/controller/InformationController.java
+++ src/main/java/com/example/starwars/contract/rest/controller/InformationController.java
@@ -3,6 +3,7 @@ package com.example.starwars.contract.rest.controller;
 import com.example.starwars.application.usecase.GetInformationUseCase;
 import com.example.starwars.domain.Model.Information;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,7 @@ public class InformationController {
 
     private final GetInformationUseCase getInformationUseCase;
     @GetMapping
+    @Cacheable(value = "information", key = "#name")
     public Information getInfo(@RequestParam ("name") String name) {
         return getInformationUseCase.getInformation(name);
     }
