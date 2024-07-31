6b485f0d62157fb64f8bee1f41c695740ac7473b
jaimegomez <jaimegfuenmayor@gmail.com>
2024-07-31 19:23:38 +0200

    git commit -m "improve Test for Controller"

--- src/test/java/com/example/starwars/contract/rest/controller/InformationControllerTest.java
+++ src/test/java/com/example/starwars/contract/rest/controller/InformationControllerTest.java
@@ -1,13 +1,17 @@
 package com.example.starwars.contract.rest.controller;
 
 import com.example.starwars.application.usecase.GetInformationUseCase;
+import com.example.starwars.domain.Model.Information;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class InformationControllerTest {
@@ -17,11 +21,31 @@ public class InformationControllerTest {
 
     @Mock
     GetInformationUseCase getInformationUseCase;
-/*
+
     @Test
     void getInfo() {
-        String name = "Luke";
-        String result = String.valueOf(this.underTest.getInfo(name));
-        assertEquals("Info" + name, result);
-    }*/
+        // Arrange
+        String name = "Luke Skywalker";
+        Information information = Information.builder()
+                .name(name)
+                .birth_year("19BBY")
+                .gender("male")
+                .planet_name("Tatooine")
+                .fastest_vehicle_driven("Speeder")
+                .films(List.of()) // Puedes agregar films si es necesario
+                .build();
+
+        when(getInformationUseCase.getInformation(name)).thenReturn(information);
+
+        //Act
+        Information result = underTest.getInfo(name);
+
+        //Assert
+        assertEquals(information.getName(), result.getName());
+        assertEquals(information.getBirth_year(), result.getBirth_year());
+        assertEquals(information.getGender(), result.getGender());
+        assertEquals(information.getPlanet_name(), result.getPlanet_name());
+        assertEquals(information.getFastest_vehicle_driven(), result.getFastest_vehicle_driven());
+        assertEquals(information.getFilms(), result.getFilms());
+    }
 }
