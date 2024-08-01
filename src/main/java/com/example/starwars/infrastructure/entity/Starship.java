8af9ca46f82bd44bca317e78fe6064c85d711b7a
jaimegomez <jaimegfuenmayor@gmail.com>
2024-08-01 14:12:09 +0200

    git commit -m "clean comments"

--- src/main/java/com/example/starwars/infrastructure/entity/Starship.java
+++ src/main/java/com/example/starwars/infrastructure/entity/Starship.java
@@ -11,7 +11,7 @@ public class Starship {
     private String manufacturer;
     private String costInCredits;
     private String length;
-    private String max_atmosphering_speed; // Nota el nombre en CamelCase
+    private String max_atmosphering_speed;
     private String crew;
     private String passengers;
     private String cargoCapacity;
